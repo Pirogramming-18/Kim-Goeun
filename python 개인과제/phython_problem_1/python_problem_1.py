bcdba75b1b98cae95e9910f02f8cee70a5c94254
김고은 <rlarhdms6828@naver.com>
2023-01-08 00:17:56 +0900

    5단계

--- python 개인과제/phython_problem_1/python_problem_1.py
+++ python 개인과제/phython_problem_1/python_problem_1.py
@@ -1,20 +1,24 @@
-num=0
+num = 0
 
-ice_n = input('부를 숫자의 개수를 입력하세요(1,2,3 만 입력가능) :')
+def player(name):
+    global num
+    ice_n = input('부를 숫자의 개수를 입력하세요(1,2,3 만 입력가능) :')
 
+    while True: #
+        if ice_n.isdigit(): #조건1 정수를 입력
+            if ice_n == '1' or ice_n =='2' or ice_n =='3':
+                break
+            else:  #1,2,3을 입력하지 않은 경우 
+                ice_n = input('1,2,3 만 입력가능 :')
+        else: # 정수를 입력하지 않은 경우
+            ice_n = input('정수만 입력가능 :')
 
-while True: #
-    if ice_n.isdigit(): #조건1 정수를 입력
-        if ice_n == '1' or ice_n =='2' or ice_n =='3':
-            break
-        else:  #1,2,3을 입력하지 않은 경우 
-            ice_n = input('1,2,3 만 입력가능 :')
-    else: # 정수를 입력하지 않은 경우
-        ice_n = input('정수만 입력가능 :')
+    ice_n= int(ice_n)
 
-ice_n= int(ice_n)
-for num in range(ice_n):
-    num = num+1
-    print ('playerA:',(num))
+    for i in range(ice_n):
+        num += 1
+        print (name ,':',num)
 
-   
+player('playerA')
+player('playerB')
+
