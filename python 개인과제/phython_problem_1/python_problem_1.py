d3cdcce884628e92cc995c05bb826121764c4cd0
김고은 <rlarhdms6828@naver.com>
2023-01-08 02:55:54 +0900

    9단계

--- python 개인과제/phython_problem_1/python_problem_1.py
+++ python 개인과제/phython_problem_1/python_problem_1.py
@@ -1,17 +1,23 @@
+from random import *
 num = 0
 
 def brGame(name):
     global num
-    ice_n = input('부를 숫자의 개수를 입력하세요(1,2,3 만 입력가능) :')
-    
-    while True: 
-        if ice_n.isdigit(): #조건1 정수를 입력
-            if ice_n == '1' or ice_n =='2' or ice_n =='3':
-                break
-            else:  #1,2,3을 입력하지 않은 경우 
-                ice_n = input('1,2,3 만 입력가능 :')
-        else: # 정수를 입력하지 않은 경우
-            ice_n = input('정수만 입력가능 :')
+   
+    if name =='computer':
+        ice_n = randint(1,3)
+
+ 
+    else: 
+        ice_n = input('부를 숫자의 개수를 입력하세요(1,2,3 만 입력가능) :')
+        while True: 
+            if ice_n.isdigit(): #조건1 정수를 입력
+                if ice_n == '1' or ice_n =='2' or ice_n =='3':
+                    break
+                else:  #1,2,3을 입력하지 않은 경우 
+                    ice_n = input('1,2,3 만 입력가능 :')
+            else: # 정수를 입력하지 않은 경우
+                ice_n = input('정수만 입력가능 :')
 
     ice_n= int(ice_n)
     for i in range(ice_n):
@@ -22,13 +28,13 @@ def brGame(name):
             break
 
 while True:            
-    brGame('playerA')
+    brGame('computer')
     if num == 31:
-        print('playerB win!')
+        print('player win!')
         break
-    brGame('playerB')
+    brGame('player')
     if num == 31:
-        print('playerA win!')
+        print('computer win!')
         break
             
 
