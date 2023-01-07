50a16e2609210678aade09ffb778c5ffb4f3e646
김고은 <rlarhdms6828@naver.com>
2023-01-07 21:50:53 +0900

    3단계

--- python 개인과제/phython_problem_1/python_problem_1.py
+++ python 개인과제/phython_problem_1/python_problem_1.py
@@ -1,3 +1,17 @@
 num=0
 
-ice_n = input('부를 숫자의 개수를 입력하세요(1,2,3 만 입력가능 :')
+ice_n = input('부를 숫자의 개수를 입력하세요(1,2,3 만 입력가능) :')
+
+
+while True: #
+    if ice_n.isdigit(): #조건1 정수를 입력
+        if ice_n == '1' or ice_n =='2' or ice_n =='3':
+            break
+        else:  #1,2,3을 입력하지 않은 경우 
+            ice_n = input('1,2,3 만 입력가능 :')
+    else: # 정수를 입력하지 않은 경우
+        ice_n = input('정수만 입력가능 :')
+
+        
+
+   
