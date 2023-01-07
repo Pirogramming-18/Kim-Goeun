e236edb9116cd7d3dcbbdf95ff2c3cb6d2efbc15
김고은 <rlarhdms6828@naver.com>
2023-01-08 01:54:12 +0900

    7단계

--- python 개인과제/phython_problem_1/python_problem_1.py
+++ python 개인과제/phython_problem_1/python_problem_1.py
@@ -3,8 +3,8 @@ num = 0
 def player(name):
     global num
     ice_n = input('부를 숫자의 개수를 입력하세요(1,2,3 만 입력가능) :')
-
-    while True: #
+    
+    while True: 
         if ice_n.isdigit(): #조건1 정수를 입력
             if ice_n == '1' or ice_n =='2' or ice_n =='3':
                 break
@@ -14,11 +14,23 @@ def player(name):
             ice_n = input('정수만 입력가능 :')
 
     ice_n= int(ice_n)
-
     for i in range(ice_n):
         num += 1
-        print (name ,':',num)
+        print(name, ':' ,num)
 
-while (num != 31 ):
+        if num == 31:
+            break
+
+while True:            
     player('playerA')
+    if num == 31:
+        print('playerB win!')
+        break
     player('playerB')
+    if num == 31:
+        print('playerA win!')
+        break
+            
+
+
+    
