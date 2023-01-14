ac14daeb185c941b63b507e14d6330fe82a866d8
김고은 <rlarhdms6828@naver.com>
2023-01-14 22:03:05 +0900

    1~11까지 구현

--- myMovieReviews/post/models.py
+++ myMovieReviews/post/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200,null=True)
-    pic = models.ImageField(upload_to='templates/image',blank=True, default='')
+    # pic = models.ImageField(upload_to='templates/image',blank=True, default='')
     year = models.IntegerField(null=True)
     genre = models.CharField(null=True, max_length=20)
     star = models.FloatField(null=True)
@@ -14,8 +14,8 @@ class Post(models.Model):
     actor = models.CharField(max_length=100, null=True)
     
 
-    def publish(self):
-        self.save()
+    # def publish(self):
+    #     self.save()
 
     def __str__(self):
         return self.title
--- myMovieReviews/post/views.py
+++ myMovieReviews/post/views.py
@@ -7,20 +7,20 @@ def post_list(request): #처음 페이지 리뷰 간략
     posts = Post.objects.all()
     return render(request, 'post/post_list.html', {'posts': posts})
 
-def post_create(request):  #게시글 새로 작성 (add movie review)
-    if request.method == "post":
-       Post.objects.create(
-            title=request.Post["title"],
-            year=request.Post["year"],
-            pic=request.Post["pic"],
-            genre=request.Post["genre"],
-            star=request.Post["star"],
-            time=request.Post["time"],
-            content=request.Post["content"],
-            director=request.Post["director"],
-            actor=request.Post["actor"],)
-       return redirect("/")
-    return render(request, "/post_create.html")
+def post_create(request, *args, **kwargs):
+  if request.method == "POST":
+    Post.objects.create(
+      title=request.POST["title"],
+      year=request.POST["year"],
+      genre=request.POST["genre"],
+      star=request.POST["star"],
+      time=request.POST["time"],
+      review=request.POST["review"],
+      director=request.POST["director"],
+      actor=request.POST["actor"],
+    )
+    return redirect("/")
+  return render(request, "post/post_create.html")
 
 def post_detail(request,pk): #게시글 상세정보 보기
     post = Post.objects.get(pk=pk)
@@ -28,22 +28,22 @@ def post_detail(request,pk): #게시글 상세정보 보기
 
 def post_update(request, pk): #게시글 수정하기 
     post = Post.objects.get(id=pk)
-    if request.method == "post":
-        post.title=request.post["title"],
-        post.year=request.post["year"],
-        post.pic=request.post["pic"],
-        post.genre=request.post["genre"],
-        post.star=request.post["star"],
-        post.time=request.post["time"],
-        post.content=request.post["content"],
-        post.director=request.post["director"],
-        post.actor=request.post["actor"],
+    if request.method == "POST":
+        post.title=request.POST["title"]
+        post.year=request.POST["year"]
+        # post.pic=request.post["pic"],
+        post.genre=request.POST["genre"]
+        post.star=request.POST["star"]
+        post.time=request.POST["time"]
+        post.review=request.POST["review"]
+        post.director=request.POST["director"]
+        post.actor=request.POST["actor"]
         post.save()
-        return redirect(f"/post/{post.id}")
+        return redirect(f"/post/{post.pk}")
     return render(request, "post/post_update.html", {"post": post})
 
 def post_delete(request, pk):  #게시글 삭제
-    if request.method == "post":
+    if request.method == "POST":
         post = Post.objects.get(id=pk)
         post.delete()
     return redirect("/")
